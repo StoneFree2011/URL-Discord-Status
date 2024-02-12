82c63cc9c8f41b4be4343af029d5ead390cfe2b6
StoneFree2011 <151025181+StoneFree2011@users.noreply.github.com>
2024-02-12 17:38:15 +0300

    HDrezka (and all mirrors) content name full support + tabs.onUpdated event changes status

--- background.js
+++ background.js
@@ -21,19 +21,63 @@ async function doRequest(text, token) {
         throw new Error("Network error occurred: " + error.message);
     }
 }
+
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-    chrome.storage.local.get(['extensionEnabled', 'token'], function (data) {
-        var extensionEnabled = data.extensionEnabled || false;
-        var token = data.token || '';
-        if (extensionEnabled) {
-            chrome.tabs.get(activeInfo.tabId, function (tab) {
-                var currentUrl = tab.url;
-                currentUrl = currentUrl.replace(/^https?:\/\//i, ""); // Удаляем приставку "http://" или "https://"
-                doRequest("Смотрит " + currentUrl, token)
-            });
-        } else {
-            doRequest(" ", token)
-        }
+    chrome.tabs.get(activeInfo.tabId, function (tab) {
+        handleTabChange(tab);
     });
 });
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (tab.active && changeInfo.url) {
+        handleTabChange(tab);
+    }
+});
+
+function handleTabChange(tab) {
+    if (!tab.url) {
+        return; // Не делаем ничего, если URL отсутствует
+    }
+
+    if (!tab.url.includes("rezka")) {
+        try {
+            var currentUrl = tab.url.replace(/^https?:\/\//i, ""); // Удаляем приставку "http://" или "https://"
+        } catch (error) {
+            throw new Error("Network error occurred: " + error.message);
+            var currentUrl = tab.url
+        }
+        chrome.storage.local.get(['extensionEnabled', 'token'], function (data) {
+            var extensionEnabled = data.extensionEnabled || false;
+            var token = data.token || '';
+            if (extensionEnabled) {
+                doRequest("Смотрит " + currentUrl, token);
+            } else {
+                doRequest(" ", token);
+            }
+        });
+    } else {
+        chrome.storage.local.get(['extensionEnabled', 'token'], function (data) {
+            var extensionEnabled = data.extensionEnabled || false;
+            var token = data.token || '';
+            if (extensionEnabled) {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    function: () => document.querySelector("h1[itemprop='name']").textContent
+                }).then(result => {
+                    var fieldValue = result[0].result;
+                    if (fieldValue) {
+                        doRequest("Смотрит '" + fieldValue + "' на HDrezka", token);
+                    } else {
+                        doRequest("Смотрит HDrezka", token);
+                    }
+                }).catch(error => {
+                    doRequest("Смотрит HDrezka", token);
+                });
+            } else {
+                doRequest(" ", token);
+            }
+        });
+    }
+}
+
 
